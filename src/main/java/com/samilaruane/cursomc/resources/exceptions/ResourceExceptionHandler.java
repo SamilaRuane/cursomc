6645514e810ac466031895e56f4a1f41518cd5df
Samila Ruane <samilaruane@gmail.com>
2017-11-22 19:39:32 -0300

    Ajuste no endpoint /categorias/{id}

---
+++ src/main/java/com/samilaruane/cursomc/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.samilaruane.cursomc.resources.exceptions;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.samilaruane.cursomc.services.exceptions.ObjectNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+	
+	@ExceptionHandler (ObjectNotFoundException.class)
+	public ResponseEntity objectNotFound (ObjectNotFoundException e, HttpServletRequest request) {
+		StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
+		
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+	}
+
+}
--- src/main/java/com/samilaruane/cursomc/services/CategoriaService.java
+++ src/main/java/com/samilaruane/cursomc/services/CategoriaService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import com.samilaruane.cursomc.domain.Categoria;
 import com.samilaruane.cursomc.repositories.CategoriaRepository;
+import com.samilaruane.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -14,6 +15,11 @@ public class CategoriaService {
 	
 	public Categoria buscar (Integer id) {
 		Categoria obj = repo.findOne(id);
+		
+		if (obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado id = " + id
+					+ ", Tipo = " + Categoria.class.getName());
+		}
 		
 		return obj;
 	}
